964f37d6ecf361d28edaf860c1f1889146e1b32c
marcDuboc <marcduboc@hotmail.com>
2023-08-19 21:11:55 +0200

    v0.1

--- sandbox/std_struct.py
+++ sandbox/std_struct.py
@@ -4,25 +4,50 @@
 # Autor: Marc DUBOC
 # Version: 11/08/2023
 
+import re
 import salome
-
+from salome.geom import geomBuilder,geomtools
 salome.salome_init()
+
+re_name= re.compile(r'_C\d+(.+?)')
 
 # Detect current study
 std_name = salome.myStudy._get_Name()
 print("Current study: ", std_name)
 
-# iterate through objects of the data tree with child iterator
+studies = salome.myStudyManager.GetOpenStudies()
+#file = salome.myStudy.path()  
+#print(file)
+
+# get object => IDL:GEOM/GEOM_Object:1.0
+def get_GEOM_Object(id):
+      return salome.myStudy.FindObjectID(id).GetObject()
+
+#get name componenent
+def get_name(id):
+      return get_SALOMEDS_SObject(id).GetName()
+
+# get object => IDL:SALOMEDS/SObject:1.0
+def get_SALOMEDS_SObject(id):
+      return geomtools.IDToSObject(id)
+
+def get_Parent(id):
+      return geomtools.IDToSObject(id).GetFather()
+
+# iterate through objects of the data tree with child iterator return child and parent id and name
 root = salome.myStudy.FindComponentID("0:1:1") # get the root component
 iter = salome.myStudy.NewChildIterator(root) # initialize from the component
 iter.InitEx(True) # init recursive mode
 while iter.More():
       c = iter.Value()
-      print(c.GetID())
+      p = get_Parent(c.GetID())
+      if re_name.match(c.GetName()):
+                # check if id exist in contact list
+                print(c.GetName(),p.GetName())
+
+      #print(c.GetID(),c.GetName(),p.GetID(),p.GetName())
       iter.Next()
       pass
 
-#TODO: get the name of the component store in dict with ID and type 
-# target find all contact and part 
 
 
--- scripts/contactAuto.py
+++ scripts/contactAuto.py
@@ -4,11 +4,15 @@
 # Autor: Marc DUBOC
 # Version: 11/08/2023
 
+# 5494 8FA2 ADF2 CAF0 4A5C A315 CA FF65
 
 import itertools
+#from copy import deepcopy
+import re
+import json
 import numpy as np
 import salome
-from salome.geom import geomBuilder,geomtools
+from salome.geom import geomBuilder, geomtools
 
 from PyQt5.QtWidgets import QWidget, QMessageBox
 from PyQt5 import QtCore, QtGui
@@ -21,37 +25,135 @@ geompy = geomBuilder.New()
 gg = salome.ImportComponentGUI("GEOM")
 salome.salome_init()
 
+# contact name regex
+re_name= re.compile(r'_C\d+(.+?)')
+
+class Contact():
+    def __init__(self,id,part,surface):
+        self.id = id
+        self.type = "bonded"  # bonded, sliding, gap
+        self.parts = [None,None]  # part name as string
+        self.surfaces = [None,None]  # surface name as string
+        self.master = None  # master number as surface index
+        self.gap = None  # gap value as float
+        self.completed= False # contact completed: both part and surface are defined
+
+        self.parts[0]=part
+        self.surfaces[0]=surface
+
+    def __str__(self):
+        return "{id: " + str(self.id) + " type: " + self.type +""
+
+    def __repr__(self) -> str:
+        return "{id: " + str(self.id) +", completed:"+str(self.completed) +", type: " + self.type + ", parts:[" + str(self.parts[0])+','+str(self.parts[1]) +"], surfaces:[" +str(self.surfaces[0])+','+str(self.surfaces[1]) +"]}"    
+    
+    def isValid(self):
+        if self.id != None:
+            return True
+        else:
+            return False
+        
+    def to_dict(self):
+        return {"id":self.id,"type":self.type,"parts":self.parts,"surfaces":self.surfaces,"master":self.master,"gap":self.gap,"completed":self.completed}
+
+    def swap_master_slave(self):
+        if self.master == 0:
+            self.master = 1
+        else:
+            self.master = 0
+
+    def add(self,part,surface):
+        if self.surfaces[0]!=surface:
+            self.parts[1]=part
+            self.surfaces[1]=surface
+            self.completed = True
+
+
+
 class AutoContact(QWidget):
     def __init__(self):
         super(AutoContact, self).__init__()
-        self.initUI()
-        self.selectParts()
+        self.contacts = dict()
         self.parts = list()
+        self.initUI()
+
+        # parse for existing contact
+        root = salome.myStudy.FindComponentID("0:1:1")
+        self.parseContact(root)
+        self.exportContact("E://GIT_REPO//SalomeUtils//debug//contact.json")
+        self.selectParts()
         
+
     def __del__(self):
         return
-    
+
+    # parse tree for contacts
+    def parseContact(self, root):
+        contact_id_list=list()
+        iter = salome.myStudy.NewChildIterator(root)
+        iter.InitEx(True)
+        while iter.More():
+            c = iter.Value()
+            pc = geomtools.IDToSObject(c.GetID()).GetFather()
+            c_name =c.GetName()
+            print(c.SubShapeID())
+            pc_name =pc.GetName()
+
+            # check if c name is valid re_name
+            if re_name.match(c_name):
+                # extract id
+                id = re.findall(r'\d+', c_name)[0]
+                # check if id exist in contact list
+                if id not in self.contacts.keys():
+                        #print('new contact',id, c_name, pc_name)
+                        new_contact = Contact(id,pc_name,c_name)
+                        self.contacts[id]=new_contact
+                        contact_id_list.append(id)
+                        
+                elif id in self.contacts.keys() and self.contacts[id].completed == False:
+                        #print("to existing contact",id, c_name, pc_name)
+                        self.contacts[id].add(pc_name,c_name)
+
+            iter.Next()
+
+    # export contact list to json file
+    def exportContact(self, filename):
+        lct = list()
+        for _,v in self.contacts.items():
+            lct.append(v.to_dict())
+        with open(filename, 'w') as f:
+            json.dump(lct,f, indent=4)
+
+
+    # get existing contact list
+    def get_id_list(self):
+        ids = list()
+        for i in range(0, len(self.contacts)):
+            ids.append(self.contacts[i].id)
+        return ids
+
     # get object => IDL:SALOMEDS/SObject:1.0
-    def get_SALOMEDS_SObject(self,id):
+    def get_SALOMEDS_SObject(self, id):
         return geomtools.IDToSObject(id)
-    
+
     # get object => IDL:GEOM/GEOM_Object:1.0
-    def get_GEOM_Object(self,id):
+    def get_GEOM_Object(self, id):
         return salome.myStudy.FindObjectID(id).GetObject()
-    
+
     def createGroup(self, parentShape, subshape, name):
         try:
-              group = geompy.CreateGroup(parentShape, geompy.ShapeType["FACE"])
-              FaceID = geompy.GetSubShapeID(parentShape, subshape)
-              geompy.AddObject(group, FaceID)
-              id_group = geompy.addToStudyInFather( parentShape, group, name )
-              return id_group
-         
+            group = geompy.CreateGroup(parentShape, geompy.ShapeType["FACE"])
+            FaceID = geompy.GetSubShapeID(parentShape, subshape)
+            geompy.AddObject(group, FaceID)
+            id_group = geompy.addToStudyInFather(parentShape, group, name)
+            return id_group
+
         except:
-              QMessageBox.critical(None,'Error',"error in create group",QMessageBox.Abort)  
+            QMessageBox.critical(
+                None, 'Error', "error in create group", QMessageBox.Abort)
 
     def initUI(self):
-        # 3D parts selected 
+        # 3D parts selected
         self.l_parts = QLabel("3D Parts for contact analysis: ", self)
         self.tb_parts = QTextBrowser()
         self.pb_loadpart = QPushButton()
@@ -72,14 +174,15 @@ class AutoContact(QWidget):
         self.sb_ctol.setValue(0.01)
         self.sb_ctol.setSingleStep(0.001)
 
-        # Common open_gmsh_options        
+        # Common open_gmsh_options
         #self.cb_common = QCheckBox("  Compound Results",self)
-        #self.cb_common.setChecked(Qt.Unchecked)
+        # self.cb_common.setChecked(Qt.Unchecked)
 
         # Ok buttons:
         self.okbox = QDialogButtonBox(self)
         self.okbox.setOrientation(Qt.Horizontal)
-        self.okbox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+        self.okbox.setStandardButtons(
+            QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
 
         # Layout:
         layout = QGridLayout()
@@ -97,49 +200,53 @@ class AutoContact(QWidget):
         self.okbox.accepted.connect(self.process)
         self.okbox.rejected.connect(self.cancel)
         self.pb_loadpart.clicked.connect(self.selectParts)
-        
+
+
+
     def selectParts(self):
         try:
-          selCount = salome.sg.SelectedCount()
-          selobj = list()
-          self.tb_parts.clear()
+            selCount = salome.sg.SelectedCount()
+            selobj = list()
+            self.tb_parts.clear()
 
-          for i in range(0, selCount):
-            sel_i=salome.sg.getSelected(i)
-            selobj_i=salome.myStudy.FindObjectID(sel_i).GetObject()
-            selobj.append(selobj_i)
-            self.tb_parts.append(selobj[i].GetName())
-          self.parts = selobj
+            for i in range(0, selCount):
+                sel_i = salome.sg.getSelected(i)
+                selobj_i = salome.myStudy.FindObjectID(sel_i).GetObject()
+                selobj.append(selobj_i)
+                self.tb_parts.append(selobj[i].GetName())
+            self.parts = selobj
 
         except:
-            QMessageBox.critical(None,'Error',"error in selected parts",QMessageBox.Abort)
+            QMessageBox.critical(
+                None, 'Error', "error in selected parts", QMessageBox.Abort)
 
-    def parseShape(self, list_shapes, kind=['PLANAR', 'PLANE','CYLINDER','CYLINDER2D','SPHERE','FACE']):
-        res= list()
+    def parseShape(self, list_shapes, kind=['PLANAR', 'PLANE', 'CYLINDER', 'CYLINDER2D', 'SPHERE', 'FACE']):
+        res = list()
         for i in range(0, len(list_shapes)):
             k = geompy.KindOfShape(list_shapes[i])
-            #print(k[0])
+            # print(k[0])
             if str(k[0]) in kind:
                 res.append(list_shapes[i])
         return res
-  
+
     def cyl_prop_to_dict(self, shape):
         """
         Convertir les proprietes en dictionnaire.
-        """       
+        """
         p = geompy.KindOfShape(shape)
-        if str(p[0]) in ('CYLINDER2D','CYLINDER'):
-            c_prop = dict(type= str(p[0]), center=(p[1],p[2],p[3]), direction=(p[4],p[5],p[6]), diameter=p[7], length=0)
+        if str(p[0]) in ('CYLINDER2D', 'CYLINDER'):
+            c_prop = dict(type=str(p[0]), center=(p[1], p[2], p[3]), direction=(
+                p[4], p[5], p[6]), diameter=p[7], length=0)
             return c_prop
         return False
 
-    def point_to_line_distance(self,point, line_point, line_dir):
+    def point_to_line_distance(self, point, line_point, line_dir):
         """
         Calcule la distance d'un point à une ligne définie par un point et une direction.
         """
         point_vec = np.array(point) - np.array(line_point)
         return np.linalg.norm(point_vec - np.dot(point_vec, line_dir) * line_dir)
-    
+
     def are_cylinders_coincident(self, shape1, shape2, gap, tolerance=0.01):
         """
         Vérifie si deux cylindres coïncident dans l'espace R^3.
@@ -147,106 +254,126 @@ class AutoContact(QWidget):
         cylinder1 = self.cyl_prop_to_dict(shape1)
         cylinder2 = self.cyl_prop_to_dict(shape2)
 
-        if cylinder1 == False  or cylinder2== False:
-           return False
-        
+        if cylinder1 == False or cylinder2 == False:
+            return False
+
         else:
-          # Normaliser les vecteurs de direction
-          dir1_normalized = np.array(cylinder1['direction']) / np.linalg.norm(cylinder1['direction'])
-          dir2_normalized = np.array(cylinder2['direction']) / np.linalg.norm(cylinder2['direction'])
-          
-          # Vérifier la colinéarité des axes
-          dir_diff = np.arccos(np.clip(np.dot(dir1_normalized, dir2_normalized), -1.0, 1.0))
-          print("dir_diff: ", dir_diff)
-          if not (np.isclose(dir_diff, 0, atol=tolerance) or np.isclose(dir_diff, np.pi, atol=tolerance)):
-              return False
-          
-          # Vérifier la coïncidence des axes
-          distance_centers_to_line1 = self.point_to_line_distance(cylinder2['center'], cylinder1['center'], dir1_normalized)
-          distance_centers_to_line2 = self.point_to_line_distance(cylinder1['center'], cylinder2['center'], dir2_normalized)
-          
-          if distance_centers_to_line1 > (cylinder1['diameter'] / 2 + tolerance) or \
-            distance_centers_to_line2 > (cylinder2['diameter'] / 2 + tolerance):
-              return False
+            # Normaliser les vecteurs de direction
+            dir1_normalized = np.array(
+                cylinder1['direction']) / np.linalg.norm(cylinder1['direction'])
+            dir2_normalized = np.array(
+                cylinder2['direction']) / np.linalg.norm(cylinder2['direction'])
 
-          # Comparer les diamètres
-          if (abs(cylinder1['diameter'] - cylinder2['diameter']) > gap):
-              return False
-          
-          return True
-        
+            # Vérifier la colinéarité des axes
+            dir_diff = np.arccos(
+                np.clip(np.dot(dir1_normalized, dir2_normalized), -1.0, 1.0))
+            print("dir_diff: ", dir_diff)
+            if not (np.isclose(dir_diff, 0, atol=tolerance) or np.isclose(dir_diff, np.pi, atol=tolerance)):
+                return False
+
+            # Vérifier la coïncidence des axes
+            distance_centers_to_line1 = self.point_to_line_distance(
+                cylinder2['center'], cylinder1['center'], dir1_normalized)
+            distance_centers_to_line2 = self.point_to_line_distance(
+                cylinder1['center'], cylinder2['center'], dir2_normalized)
+
+            if distance_centers_to_line1 > (cylinder1['diameter'] / 2 + tolerance) or \
+                    distance_centers_to_line2 > (cylinder2['diameter'] / 2 + tolerance):
+                return False
+
+            # Comparer les diamètres
+            if (abs(cylinder1['diameter'] - cylinder2['diameter']) > gap):
+                return False
+
+            return True
+
     def process(self):
-      selCount=0
-      num_cont = 0
-      gap = eval(str(self.sb_gap.text()))
-      isOk = False
-      ss_isOk = False     
+        selCount = 0
+        num_cont = 0
+        gap = eval(str(self.sb_gap.text()))
+        isOk = False
+        ss_isOk = False
 
-      try:
-        selobj = self.parts
-        selCount = len(selobj)
-        
-      except:
-        QMessageBox.critical(None,'Error',"Select 2 or more parts 3D first",QMessageBox.Abort)
+        try:
+            selobj = self.parts
+            selCount = len(selobj)
 
-      if selCount > 1:
-        # combine all possible solid pairs
-        combine = list(itertools.combinations(selobj,2))
+        except:
+            QMessageBox.critical(
+                None, 'Error', "Select 2 or more parts 3D first", QMessageBox.Abort)
 
-        for i in range(0, len(combine)):
-            try:
-                isOk, res1, res2 = geompy.FastIntersect(combine[i][0], combine[i][1], gap)
+        if selCount > 1:
+            # combine all possible solid pairs
+            combine = list(itertools.combinations(selobj, 2))
 
-            except:
-                isOk = False
-                #QMessageBox.critical(None,'Error 1',"Unexpected error",QMessageBox.Abort)
+            for i in range(0, len(combine)):
+                try:
+                    isOk, res1, res2 = geompy.FastIntersect(
+                        combine[i][0], combine[i][1], gap)
 
-            if isOk:
-                CONT  = geompy.SubShapes(combine[i][0], res1)
-                CONT2 = geompy.SubShapes(combine[i][1], res2)
-                
-                # check if object are planar or cylinder
-                cont_valid = self.parseShape(CONT)
-                cont2_valid = self.parseShape(CONT2)
+                except:
+                    isOk = False
+                    #QMessageBox.critical(None,'Error 1',"Unexpected error",QMessageBox.Abort)
 
-                # combine all possible subshapes
-                comb_sub = list(itertools.product(cont_valid, cont2_valid))
+                if isOk:
+                    CONT = geompy.SubShapes(combine[i][0], res1)
+                    CONT2 = geompy.SubShapes(combine[i][1], res2)
 
-                # check if subshapes intersect
-                for c in comb_sub:
-                    try:
-                      ss_isOk, _, _ = geompy.FastIntersect(c[0], c[1], gap)
+                    # check if object are planar or cylinder
+                    cont_valid = self.parseShape(CONT)
+                    cont2_valid = self.parseShape(CONT2)
 
-                    except:
-                       ss_isOk = False
-                       
-                    if ss_isOk:
-                      common1 = geompy.MakeCommon(c[0], c[1])
-                      props = geompy.BasicProperties(common1)
-                      area_com = props[1]
+                    # combine all possible subshapes
+                    comb_sub = list(itertools.product(cont_valid, cont2_valid))
 
-                      if (area_com > 0.0) or (self.are_cylinders_coincident(c[0], c[1], gap)==True):
-                        num_cont += 1
-                  
-                        name_group_1 = 'C' + str(num_cont) + '_A'
-                        grp_id = self.createGroup(combine[i][0],c[0],name_group_1)
-                        gg.setColor(grp_id, 255,0,0)
+                    # check if subshapes intersect
+                    for c in comb_sub:
+                        try:
+                            ss_isOk, _, _ = geompy.FastIntersect(c[0], c[1], gap)
 
-                        name_group_2 = 'C' + str(num_cont) + '_B'
-                        grp_id = self.createGroup(combine[i][1],c[1],name_group_2)
-                        gg.setColor(grp_id, 0,255,0)
+                        except:
+                            ss_isOk = False
 
+                        if ss_isOk:
+                            common = geompy.MakeCommon(c[0], c[1])
+                            props = geompy.BasicProperties(common)
+                            area_com = props[1]
 
-        msg_cont = "nb contacts : " + str(num_cont)    
-        QMessageBox.information(None, "Information", msg_cont, QMessageBox.Ok)
-            
-      if salome.sg.hasDesktop():
-        salome.sg.updateObjBrowser()
-	  
+                            if (area_com > 0.0) or (self.are_cylinders_coincident(c[0], c[1], gap) == True):
+                                num_cont += 1
+                                contacts_list= [int(x) for x in self.contacts.keys()]
+                                if len(contacts_list) == 0:
+                                    id = 1
+                                else:
+                                    id = max(contacts_list)+1
+
+                                name_group_1 = '_C' + str(id) + 'A'
+                                grp_id = self.createGroup(
+                                    combine[i][0], c[0], name_group_1)
+                                gg.setColor(grp_id, 255, 0, 0)
+
+                                name_group_2 = '_C' + str(id) + 'B'
+                                grp_id = self.createGroup(
+                                    combine[i][1], c[1], name_group_2)
+                                gg.setColor(grp_id, 0, 255, 0)
+
+                                # create contact
+                                new_contact = Contact(id, combine[i][0].GetName(), c[0].GetName())
+                                new_contact.add(combine[i][1].GetName(), c[1].GetName())
+                                self.contacts[str(id)]=(new_contact)
+
+            msg_cont = "nb contacts : " + str(num_cont)
+            print(self.contacts)
+            QMessageBox.information(None, "Information", msg_cont, QMessageBox.Ok)
+
+        if salome.sg.hasDesktop():
+            salome.sg.updateObjBrowser()
+
     # cancel function
     def cancel(self):
         self.close()
         d.close()
+
 
 d = QDockWidget()
 d.setWidget(AutoContact())
