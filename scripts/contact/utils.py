ed3580207006b737bcef8cdc4321941f5cb219e7
marcDuboc <marcduboc@hotmail.com>
2023-08-29 14:35:47 +0200

    add rename

--- scripts/contact/utils.py
+++ scripts/contact/utils.py
@@ -5,14 +5,31 @@
 # Version: 11/08/2023
 
 import salome
-from salome.geom import geomtools
+builder = salome.myStudy.NewBuilder()
 
 # get object => IDL:SALOMEDS/SObject:1.0
 # id as "0:1:1..."
 def get_SALOMEDS_SObject(id):
-    return geomtools.IDToSObject(id)
+    return salome.IDToSObject(id)
 
 # get object => IDL:GEOM/GEOM_Object:1.0
 # id as "0:1:1..."
 def get_GEOM_Object(id):
-    return salome.myStudy.FindObjectID(id).GetObject()
+    return salome.IDToObject(id)
+
+# convert object => IDL:GEOM/GEOM_Object:1.0
+def convertObjToSobj(obj):
+    return salome.ObjectToSOBJ(obj)
+
+#rename object in study and geom from itself
+def rename_from_obj(obj, name):
+    obj.SetName(name)
+    sobj=salome.ObjectToSObject(obj)
+    builder.FindOrCreateAttribute(sobj, "AttributeName").SetValue(name)
+
+#rename object in study and geom from id
+def rename_from_id(id, name):
+    obj = salome.IDToObject(id)
+    obj.SetName(name)
+    sobj=salome.ObjectToSObject(obj)
+    builder.FindOrCreateAttribute(sobj, "AttributeName").SetValue(name)
