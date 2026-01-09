fe15fbd8b51d47185e4f91619cc4f21a591ddbe7
marcDuboc <marcduboc@hotmail.com>
2026-01-09 11:34:14 +0100

    autocreate log dir and log files if missing

--- scripts/common/__init__.py
+++ scripts/common/__init__.py
@@ -2,12 +2,22 @@ import os
 import inspect
 import logging
 
-#Logging
-PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-LOG_FILE = os.path.join(PATH, '..' ,'log', 'debug.log')
-LOG_LEVEL = logging.DEBUG
-logging.basicConfig(filename=LOG_FILE, level=LOG_LEVEL, format='%(asctime)s %(levelname)s %(message)s')
+# Base path
+BASE_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+# Logging
+LOG_DIR = os.path.join(BASE_DIR, '..', 'log')
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR, 'debug.log')
+if not os.path.exists(LOG_FILE):
+    open(LOG_FILE, 'a').close()
+    
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
 
 #Gui image folder
-ROOT_PATH= os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-IMG_PATH = os.path.join(ROOT_PATH, 'img')
+IMG_PATH = os.path.join(BASE_DIR, 'img')
